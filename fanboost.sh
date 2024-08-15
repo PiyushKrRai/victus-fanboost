020854e8fb149dc38d863633979750717d691dfe
Ilgın Kanat <48878763+nightfullstar@users.noreply.github.com>
2024-08-16 00:51:09 +0400

    Update fanboost.sh

--- fanboost.sh
+++ fanboost.sh
@@ -7,9 +7,9 @@ PWM1_ENABLE_STATUS=$(cat /sys/devices/platform/hp-wmi/hwmon/hwmon*/pwm1_enable)
 if [[ $PWM1_ENABLE_STATUS -eq "2" ]]; then
     echo "Enabling fan boost..."
     echo "0" | sudo tee /sys/devices/platform/hp-wmi/hwmon/hwmon*/pwm1_enable > /dev/null
-    echo "Fan boost has been disabled."
+    echo "Fan boost has been enabled."
 else
     echo "Disabling fan boost..."
     echo "2" | sudo tee /sys/devices/platform/hp-wmi/hwmon/hwmon*/pwm1_enable > /dev/null
-    echo "Fan boost has been enabled."
+    echo "Fan boost has been disabled."
 fi
